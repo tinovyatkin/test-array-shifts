2aaa8e93ccc7b6438eb2aed707d4f0de3235a1e3
Konstantin Vyatkin <tino@vtkn.io>
2020-04-16 21:36:10 -0400

    tune CLI a little

--- cli.js
+++ cli.js
@@ -6,12 +6,16 @@ const { readdir } = require("fs").promises;
 async function cli() {
   // get our shift function options
   const files = (await readdir(__dirname, { withFileTypes: true }))
-    .filter((file) => file.isFile() && file.name.startsWith("shift-"))
+    .filter(
+      (file) => file.isFile() && /^shift-.+(?<!\.test)\.js$/i.test(file.name)
+    )
     .map(({ name }) => name);
 
   // ask which one is preferred by user
   const ui = new inquirer.ui.BottomBar();
-  while (true) {
+  // to output nice message in English
+  const pluralRules = new Intl.PluralRules("en-US");
+  do {
     const shiftOption = await inquirer.prompt([
       {
         name: "module",
@@ -36,10 +40,9 @@ async function cli() {
          * @param {number[]} input
          */
         validate(input) {
-          ui.log.write(`${input}: ${typeof input}, ${Array.isArray(input)}`);
+          if (input.length < 1) return "Please enter at least one number";
           // check that we have all numbers as power of 2
           // any power of 2 number must become 0 on once bitwise left shift (see README)
-          if (input.length < 1) return "Please enter at least one number";
           if (input.some((n) => n < 1 || n & (n - 1)))
             return "All numbers must be power of 2";
 
@@ -49,23 +52,24 @@ async function cli() {
     ]);
 
     // load selected function implementation
-    const shiftCounter = require(`./${shiftOption.module}`);
-
+    const shifts = require(`./${shiftOption.module}`)(shiftOption.numbers);
     ui.log.write(
-      `Array [${shiftOption.numbers.join(", ")}] requires ${shiftCounter(
-        shiftOption.numbers
-      )} shifts`
+      `Array [${shiftOption.numbers.join(", ")}] minimizes in ${shifts} shift${
+        pluralRules.select(shifts) === "one" ? "" : "s"
+      }`
     );
 
     // ask to repeat?
-    const repeat = await inquirer.prompt({
-      type: "confirm",
-      name: "repeat",
-      message: "Repeat?",
-      default: false,
-    });
-    if (!repeat.repeat) break;
-  }
+  } while (
+    (
+      await inquirer.prompt({
+        type: "confirm",
+        name: "repeat",
+        message: "Repeat?",
+        default: false,
+      })
+    ).repeat
+  );
 }
 
 cli();
